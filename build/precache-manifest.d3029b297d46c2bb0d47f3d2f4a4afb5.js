404ac1499d939abf583a6040a460ebdd6cefd94e
pkzone <prakash>
2022-02-25 20:04:45 +0545

    committed

---
+++ build/precache-manifest.d3029b297d46c2bb0d47f3d2f4a4afb5.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "dd051bdce5c39b07bebb",
+    "url": "/static/css/main.c38bd704.chunk.css"
+  },
+  {
+    "revision": "dd051bdce5c39b07bebb",
+    "url": "/static/js/main.dd051bdc.chunk.js"
+  },
+  {
+    "revision": "41211fcf000cf3a9973c",
+    "url": "/static/js/1.41211fcf.chunk.js"
+  },
+  {
+    "revision": "d63517b5062df230165e",
+    "url": "/static/css/2.e629488c.chunk.css"
+  },
+  {
+    "revision": "d63517b5062df230165e",
+    "url": "/static/js/2.d63517b5.chunk.js"
+  },
+  {
+    "revision": "39a7286a869c5b2ec7f4",
+    "url": "/static/js/runtime~main.39a7286a.js"
+  },
+  {
+    "revision": "248a7c88c5d6af20952ec52530ca77ad",
+    "url": "/static/media/abatar.248a7c88.webp"
+  },
+  {
+    "revision": "2a58dcf83e90477424fff1c7db445df0",
+    "url": "/static/media/php.2a58dcf8.webp"
+  },
+  {
+    "revision": "de6473c74e6a036edb6df477247a5bcb",
+    "url": "/static/media/nodejs.de6473c7.webp"
+  },
+  {
+    "revision": "b771e247bf231151be1397bd4bbde480",
+    "url": "/static/media/python.b771e247.webp"
+  },
+  {
+    "revision": "35663a0147a20f8b33b61373d0e81b7e",
+    "url": "/static/media/rails.35663a01.webp"
+  },
+  {
+    "revision": "cb01ceeaabacecf1c3f27d6112d36cd7",
+    "url": "/static/media/chevron-right.cb01ceea.svg"
+  },
+  {
+    "revision": "76ac3922efb6d51c20d9a5701cec2321",
+    "url": "/static/media/bg.76ac3922.webp"
+  },
+  {
+    "revision": "87534e9856356744ca959be3bb808658",
+    "url": "/index.html"
+  }
+];
