49b3acbbcf721d6faa3e9c8fbbb44646695c1f1d
pkzone <pt6769759@gmail.com>
2025-07-06 20:39:31 +0300

    making better

--- src/com/tabs.js
+++ src/com/tabs.js
@@ -1,35 +1,35 @@
 import React from 'react';
-
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import 'react-tabs/style/react-tabs.css';
+import { BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom';
 import Home from './inner/home';
 import Poftfolio from './inner/poftfolio';
 import About from './inner/about';
 import Contact from './inner/contact';
 
-export default () => (
- <div className='tabs-container'>
-<Tabs>
-    <TabList>
-      <Tab>Home</Tab>
-      <Tab>Portfolio</Tab>
-      <Tab>About</Tab>
-      <Tab>Contact</Tab>
-    </TabList>
-
-    <TabPanel>
-      <Home/>
-    </TabPanel>
-    <TabPanel>
-      <Poftfolio/>
-    </TabPanel>
-    <TabPanel>
-      <About/>
-    </TabPanel>
-    <TabPanel>
-      <Contact/>
-    </TabPanel>
-  </Tabs>
- </div>
-  
+const Navbar = () => (
+  <nav className="navbar">
+    <div className="navbar-logo">prakash360</div>
+    <ul>
+      <li><Link to="/home">Home</Link></li>
+      <li><Link to="/portfolio">Portfolio</Link></li>
+      <li><Link to="/about">About</Link></li>
+      <li><Link to="/contact">Contact</Link></li>
+    </ul>
+  </nav>
 );
+
+export default function Tabs() {
+  return (
+    <Router>
+      <Navbar />
+      <div className="page-container" style={{ marginTop: '70px' }}>
+        <Routes>
+          <Route path="/" element={<Navigate to="/home" replace />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/portfolio" element={<Poftfolio />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
